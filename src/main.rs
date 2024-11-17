520d3a9a4afc0e86e84b8dbb1d62216d3c111297
zimrustpro <zimrustpro@gmail.com>
2024-11-17 11:02:22 +0200

    While loop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 fn main() {
     let mut counter = 0;
-    let mut counter2 = 0;
-    println!("Now entering the first loop.");
-    'first_loop: loop {
+    while counter < 5 {
         counter += 1;
-        println!("The counter is now {}", counter);
-        if counter > 5 {
-            println!("Now entering the second loop.");
-            'second_loop: loop {
-                println!("The second counter is now {}", counter2);
-                counter2 += 1;
-                if counter2 == 3 { break 'first_loop; }
-            }
-        }
+        println!("The counter is now: {counter}")
     }
 }
