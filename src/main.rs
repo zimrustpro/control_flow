58ffbc8b1d13d95f156a98e8c333d7e9ef12343e
zimrustpro <zimrustpro@gmail.com>
2024-11-17 11:16:11 +0200

    Compare colors using control flow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 fn main() {
-    let mut counter = 5;
-    let my_number = loop {
-        counter += 1;
-        if counter % 53 == 3 {
-            break counter;
+    let first = (200, 0, 0);
+    let second = (50, 50, 50);
+    let third = (200, 50, 0);
+
+    match_colors(first);
+    match_colors(second);
+    match_colors(third);
+}
+
+fn match_colors(rbg: (i32, i32, i32)) {
+    let (red, blue, green) = (rbg.0, rbg.1, rbg.2);
+    println!("Comparing a color with {} red {} blue and {} green", red, blue, green);
+    let color_vec = vec![(red, "red"), (blue, "blue"), (green, "green")];
+    let mut all_have_at_least_10 = true;
+    for (amount, color) in color_vec {
+        if amount < 10 {
+            all_have_at_least_10 = false;
+            println!("Not much {}", color);
         }
-    };
-    println!("{}", my_number);
+    }
+    if all_have_at_least_10 {
+        println!("Each color has at least 10.")
+    }
+    println!()
 }
