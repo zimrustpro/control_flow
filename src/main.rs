3027ead7c404aafc1c055e5b2e1e9e6771d56133
zimrustpro <zimrustpro@gmail.com>
2024-11-17 11:00:57 +0200

    Give a loop a name

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
     let mut counter = 0;
-    loop {
+    let mut counter2 = 0;
+    println!("Now entering the first loop.");
+    'first_loop: loop {
         counter += 1;
-        println!("The counter is now: {counter}");
-        if counter == 5 { break; }
+        println!("The counter is now {}", counter);
+        if counter > 5 {
+            println!("Now entering the second loop.");
+            'second_loop: loop {
+                println!("The second counter is now {}", counter2);
+                counter2 += 1;
+                if counter2 == 3 { break 'first_loop; }
+            }
+        }
     }
 }
