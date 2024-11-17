643200c8e9fab30c7991b1bb2bf559801d6cf577
zimrustpro <zimrustpro@gmail.com>
2024-11-17 11:04:44 +0200

    For loop

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ fn main() {
     let mut counter = 0;
     while counter < 5 {
         counter += 1;
-        println!("The counter is now: {counter}")
+        println!("The counter is now: {counter}");
+    }
+    println!();
+    for number in 0..3 {
+        println!("The number is {number}");
+    }
+    println!();
+    for number in 0..=3 {
+        println!("The next number is {number}");
+    }
+    println!();
+    for _ in 0..3 {
+        println!("Printing the same thing three times");
     }
 }
