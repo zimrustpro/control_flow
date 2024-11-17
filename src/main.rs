9f6a88e745f7a5fabdc03438297efb29dee1b378
zimrustpro <zimrustpro@gmail.com>
2024-11-17 10:16:46 +0200

    Basic control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let my_number = 5;
+    if my_number == 7 {
+        println!("It's seven");
+    } else if my_number == 6 {
+        println!("It's six");
+    } else {
+        println!("It's a different number");
+    }
+
+    if my_number % 2 == 1 && my_number > 0 {
+        println!("It's a positive odd number");
+    } else if my_number == 6 {
+        println!("It's six");
+    } else {
+        println!("It's a different number");
+    }
 }
